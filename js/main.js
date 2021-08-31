cb16662c2807152f2132ba0d0bee26c1402cf38e
edo <mdinh@hawk.iit.edu>
2021-08-30 21:39:20 -0500

    feat: update stuff

---
+++ js/main.js
@@ -0,0 +1,22 @@
+function onClickGetMeme() {
+  const memeImage = document.getElementById("meme-image");
+  const memeError = document.getElementById("meme-error");
+  memeError.innerText = "";
+
+  let request = new XMLHttpRequest();
+  request.open("GET", "https://meme-api.herokuapp.com/gimme/memes");
+  request.send();
+  request.onload = () => {
+    if (request.status === 200) {
+      memeImage.src = JSON.parse(request.response).url;
+      memeImage.onload = function () {
+        if (this.width >= window.innerWidth / 3) {
+          memeImage.style.width = window.innerWidth / 3 + "px";
+          memeImage.style.height = "auto";
+        }
+      };
+    } else {
+      memeError = request.statusText;
+    }
+  };
+}
